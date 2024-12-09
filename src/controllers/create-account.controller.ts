ad565b06a731b1253c268ff7d47ad5ec661d45f7
Pablo Eliezer <pabloeliezer@hotmail.com>
2024-12-09 20:52:42 -0300

    Install zod and fromZodError to validate email when create user account though pipes

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -1,12 +1,23 @@
-import { Body, ConflictException, Controller, Post } from '@nestjs/common';
+import { Body, ConflictException, Controller, Post, UsePipes } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { z } from 'zod';
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe';
+
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string().min(6),
+});
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>;
 
 @Controller('accounts')
 export class CreateAccountController {
   constructor(private prisma: PrismaService) {}
 
-  @Post('')
-  async handle(@Body() body: any) {
+  @Post()
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  async handle(@Body() body: CreateAccountBodySchema): Promise<any> {
     const { name, email, password } = body;
 
     const userWithSameEmail = await this.prisma.user.findUnique({
