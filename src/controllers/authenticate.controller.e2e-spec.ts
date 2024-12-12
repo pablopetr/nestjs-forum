0f6de865abeb69d791d388c76179ba289d050ea6
Pablo Eliezer <pabloeliezer@hotmail.com>
2024-12-11 21:23:55 -0300

    Create Authenticate and CreateAccount controller tests

---
+++ src/controllers/authenticate.controller.e2e-spec.ts
@@ -0,0 +1,52 @@
+import { AppModule } from '@/app.module';
+import { Test } from '@nestjs/testing';
+import request from 'supertest';
+import { INestApplication } from '@nestjs/common';
+import { PrismaService } from '@/prisma/prisma.service';
+import { hash } from 'bcryptjs';
+
+describe('Authenticate (E2E)', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    })
+      .compile();
+
+    app = moduleRef.createNestApplication();
+
+    prisma = moduleRef.get(PrismaService);
+
+    await app.init();
+  });
+
+  test('[POST] /sessions', async () => {
+    await prisma.user.create({
+      data: {
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: await hash('password', 8),
+      }
+    });
+
+    const userOnDatabase = await prisma.user.findUnique({
+      where: {
+        email: 'johndoe@example.com',
+      }
+    })
+
+    const response = await request(app.getHttpServer())
+      .post('/sessions')
+      .send({
+        email: 'johndoe@example.com',
+        password: 'password',
+      });
+
+
+    expect(response.statusCode).toBe(201);
+
+    expect(userOnDatabase).toBeTruthy();
+  });
+});
